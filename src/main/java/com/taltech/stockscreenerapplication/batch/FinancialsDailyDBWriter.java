ac51aa0a6566e5b4ce6ac5a6aa05fa6d61931d3f
Marko Jagor <majago@ttu.ee>
2020-04-07 12:40:06 +0300

    Database writers

---
+++ src/main/java/com/taltech/stockscreenerapplication/batch/FinancialsDailyDBWriter.java
@@ -0,0 +1,21 @@
+package com.taltech.stockscreenerapplication.batch;
+
+import com.taltech.stockscreenerapplication.model.FinancialsDaily;
+import com.taltech.stockscreenerapplication.repository.FinancialsDailyRepository;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class FinancialsDailyDBWriter implements ItemWriter<FinancialsDaily> {
+
+    @Autowired
+    private FinancialsDailyRepository financialsDailyRepository;
+
+    @Override
+    public void write(List<? extends FinancialsDaily> financialsDailies) throws Exception {
+        financialsDailyRepository.saveAll(financialsDailies);
+    }
+}
