760d189cef9a80197cccdc1613902dbfae9f993f
Marko Jagor <majago@ttu.ee>
2020-04-23 21:09:11 +0300

    Deleted all unused ticker and valuation classes

--- src/main/java/com/taltech/stockscreenerapplication/repository/TickerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taltech.stockscreenerapplication.repository;
-
-import com.taltech.stockscreenerapplication.model.Ticker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TickerRepository extends JpaRepository<Ticker, Long> {
-}
--- src/main/java/com/taltech/stockscreenerapplication/repository/ValuationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taltech.stockscreenerapplication.repository;
-
-import com.taltech.stockscreenerapplication.model.Valuation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ValuationRepository extends JpaRepository<Valuation, Long> {
-}
