62a83e63a3ea526855aa24c95e27012431f44649
Marko Jagor <majago@ttu.ee>
2020-05-07 13:58:01 +0300

    Refactor code

--- src/test/java/com/taltech/stockscreenerapplication/StockScreenerApplicationTests.java
+++ src/test/java/com/taltech/stockscreenerapplication/StockScreenerApplicationTests.java
@@ -1,11 +1,10 @@
 package com.taltech.stockscreenerapplication;
 
 import com.taltech.stockscreenerapplication.controller.CompanyDimensionController;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class StockScreenerApplicationTests {
@@ -15,7 +14,7 @@ class StockScreenerApplicationTests {
 
     @Test
     void contextLoads() {
-        assertThat(companyDimensionController).isNotNull();
+        Assert.assertNotNull(companyDimensionController);
     }
 
 }
