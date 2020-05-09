ad919e6e2cd3abc6c69e37ce3bddffb57b76f310
Marko Jagor <majago@ttu.ee>
2020-05-09 20:30:20 +0300

    Add 404 response to null company object

--- src/main/java/com/taltech/stockscreenerapplication/controller/CompanyDimensionController.java
+++ src/main/java/com/taltech/stockscreenerapplication/controller/CompanyDimensionController.java
@@ -3,12 +3,12 @@ package com.taltech.stockscreenerapplication.controller;
 import com.taltech.stockscreenerapplication.model.CompanyDimension;
 import com.taltech.stockscreenerapplication.repository.CompanyDimensionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/companies")
@@ -24,8 +24,9 @@ public class CompanyDimensionController {
     }
 
     @GetMapping(value = "/{id}")
-    public Optional<CompanyDimension> getCompany(@PathVariable String id) {
+    public CompanyDimension getCompany(@PathVariable String id) {
 
-        return companyDimensionRepository.findById(id);
+        return companyDimensionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find company by id: " + id));
     }
 }
