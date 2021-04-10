60ca3c85c98b416452414d7b831a7ce7fbcc81d6
MaJogi <47116426+MaJogi@users.noreply.github.com>
2021-04-10 17:35:28 +0300

    Updated constants

--- src/main/java/com/taltech/stockscreenerapplication/Constants.java
+++ src/main/java/com/taltech/stockscreenerapplication/Constants.java
@@ -1,6 +1,8 @@
 package com.taltech.stockscreenerapplication;
 
 public class Constants {
-    int EXAMPLE = 9000;
-    String EXAMPLE_URL = "src/main/resources/csv/tkm-2017_q2_CSV_modified_by_frontend.csv";
+    public static int EXAMPLE = 9000;
+    public static String EXAMPLE_URL = "src/main/resources/csv/tkm-2017_q2_CSV_modified_by_frontend.csv";
+    public static String FILENAME = "src/main/resources/csv/tkm-2017_q2_CSV_modified_by_frontend.csv";
+    public static String TESTFIRM = "TKM1T";
 }
